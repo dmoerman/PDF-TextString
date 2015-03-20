2e4c385aa1dbdb23b54c8fbc7537c8df8a04f770
Moerman Dylan <dylanmoerman@gmail.com>
2015-03-20 14:01:53 +0100

    added new prototype functions and pdftotext implementation

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 var text = require('./PDF-text.js'),
+    fonts= require('./PDF-fonts.js'),
     fs = require('fs');
 
 function PDF() {
     if (false === (this instanceof PDF)) {
-        binaryPaths = [];
         return new PDF();
     }
 }
 
 PDF.prototype.setBinaryPath_PdfToText = function(binary_path){
-    PDF.binaryPaths.push({"pdftotext" : binary_path});
+    PDF.binaryPaths_PdfToText = binary_path;
 };
 
 PDF.prototype.setBinaryPath_PdfFont = function(binary_path){
-    PDF.binaryPaths.push({"pdffont" : binary_path});
+    PDF.binaryPath_PdfFont = binary_path;
+};
+
+PDF.prototype.hasText = function (pdf_path, cb){
+    fs.exists(pdf_path, function (exist) {
+        if (!exist) return cb('no file exists at the path you specified');
+    })
 };
 
 PDF.prototype.pdftotext = function (pdf_path, options, cb) {
 
     fs.exists(pdf_path, function (exist) {
         if (!exist) return cb('no file exists at the path you specified');
-        text.extract(pdf_path, options, cb, binaryPaths)
+        this.prototype.hasText(pdf_path, function(bool){
+            (bool) ? text.extract(pdf_path, options, cb , PDF.binaryPaths_PdfToText): cb(null,null);
+        });
+
     });
 };
 
